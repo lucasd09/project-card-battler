e21691a1104100b864a5968b2bf6b27c30676a97
lucas dalan <lucasdalan09@gmail.com>
2025-09-02 20:10:53 -0300

    feat: require auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/fn/require-auth";
 import { authClient } from "@/lib/auth-client";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: App,
   beforeLoad: async () => {
-    const data = await authClient.getSession();
+    const data = await requireAuth();
 
     if (!data) {
       throw redirect({ to: '/login' })
     }
+
+    return data;
   }
 });
 
 function App() {
-  const { data } = authClient.useSession();
+
+  const { data } = authClient.useSession()
 
   const handleLogout = async () => {
     console.log('logging out');
 
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          redirect({ to: '/login' })
-        },
-      },
-    });
+    await authClient.signOut({});
+
+    return redirect({ to: '/login' });
   }
   return (
     <div className="text-center">
